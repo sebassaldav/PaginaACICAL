7c240ccd1f24247f5649aa18f314fac3bc558687
Sebastian Saldarriaga <js.sebas5@gmail.com>
2025-12-22 17:21:41 -0500

    Update: visuals in page, footer, add date in convocatoria, change colors in regimen

--- src/app/contacto/contacto.component.spec.ts
+++ src/app/contacto/contacto.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContactoComponent } from './contacto.component';
 
-import { FooterComponent } from './contacto.component';
-
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('ContactoComponent', () => {
+  let component: ContactoComponent;
+  let fixture: ComponentFixture<ContactoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ ContactoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(ContactoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
